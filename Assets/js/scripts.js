4908c55071f07e9b3f233a513e753a2cb625043c
FTembe <31000619+FTembe@users.noreply.github.com>
2021-04-19 12:27:46 +0200

    first commit

---
+++ Assets/js/scripts.js
@@ -0,0 +1,49 @@
+let currentImage = 0,
+  menu_item = document.querySelectorAll(".link-items span"),
+  images = document.querySelectorAll(".slider img"),
+  imagesLength = images.length,
+  dot = document.querySelectorAll(".circle"),
+  slider_options = document.querySelector(".slider"),
+  time = slider_options.getAttribute("time")
+    ? slider_options.getAttribute("time")
+    : 5000;
+
+function nextImage(index) {
+  activeContent();
+}
+
+function activeContent() {
+  dot[currentImage].classList.remove("active");
+  images[currentImage].classList.remove("active");
+  currentImage++;
+  if (currentImage == imagesLength) {
+    currentImage = 0;
+  }
+
+  images[currentImage].classList.add("active");
+  dot[currentImage].classList.add("active");
+}
+
+function start() {
+  setInterval(() => {
+    nextImage();
+  }, time);
+}
+
+window.addEventListener("load", start);
+
+menu_item.forEach((value, index) => {
+  value.addEventListener("click", () => {
+    item(index);
+  });
+});
+
+function item(elem) {
+  menu_item.forEach((value, index) => {
+    if (elem == index) {
+      value.classList.add("active");
+    } else {
+      value.classList.remove("active");
+    }
+  });
+}
