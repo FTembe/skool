d80da2c8cdac4c9db2b34977920b311b1c0fab58
FTembe <31000619+FTembe@users.noreply.github.com>
2021-05-20 15:41:28 +0200

    accordion section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-
+// menu
   let menu =  document.querySelector('.menu');
    document.querySelector('#search').addEventListener('click',()=>{
 alert('relaxa');
@@ -9,3 +9,25 @@ alert('relaxa');
     document.querySelector('.menu-close').addEventListener('click',()=>{
 menu.classList.remove('active');
     })
+// accordion-button-course
+
+      document.querySelectorAll('.accordion-button-course ').forEach((button, i)=>{
+
+    button.addEventListener('click',()=>{
+
+      let accordion_content = button.nextElementSibling,
+      accordion_height = accordion_content.scrollHeight;
+      button.classList.toggle('active');
+      console.log((button.classList.contains('active')));
+
+      if(button.classList.contains('active')){
+
+        accordion_content.style.maxHeight=accordion_height+'px';
+
+        document.body.scrollHeight+accordion_height+'px';
+      }else{
+       accordion_content.style.maxHeight=0;
+      }
+    })
+
+  });
